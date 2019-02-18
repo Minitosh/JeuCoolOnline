5c4ab6085682847e90b21faf947628b3390ffbef
Minitosh <marc.deshayes44@gmail.com>
2019-02-18 15:58:02 +0100

    Update Imports

--- Monster.js
+++ Monster.js
@@ -1,4 +1,6 @@
-class Monster extends Actor {
+import Actor from "./Actor.js"
+
+export default class Monster extends Actor {
     constructor(name, strenght, pv, image) {
         super(name, strenght, pv, image); // appelle le constructeur parent avec le paramètre
     }
--- Player.js
+++ Player.js
@@ -1,4 +1,6 @@
-class Player extends Actor {
+import Actor from "./Actor.js"
+
+export default class Player extends Actor {
     constructor(name, strenght, pv, image) {
         super(name, strenght, pv, image); // appelle le constructeur parent avec le paramètre
     }
