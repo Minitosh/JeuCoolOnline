bb69d28b542045b74f9c4098a4fc965c3703cbb6
Minitosh <marc.deshayes44@gmail.com>
2019-02-18 14:36:19 +0100

    Update Actor.js et Player.js et Monster.js

--- Monster.js
+++ Monster.js
@@ -2,6 +2,8 @@ class Monster extends Actor {
     constructor(name, image) {
         super(name, image); // appelle le constructeur parent avec le paramètre
     }
+
     fight() {
         console.log("fight-Monster")
     }
+}
