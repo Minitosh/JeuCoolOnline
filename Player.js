aaee564b17518f593d43c3c3e488e16eb6b7c1d9
Minitosh <marc.deshayes44@gmail.com>
2019-02-18 14:37:33 +0100

    Update Player.js

---
+++ Player.js
@@ -0,0 +1,9 @@
+class Player extends Actor {
+    constructor(name, image) {
+        super(name, image); // appelle le constructeur parent avec le paramètre
+    }
+
+    fight() {
+        console.log("fight-Monster")
+    }
+}
