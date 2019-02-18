c93f809a434695b891e551fa7049e5d0ad0f396e
Minitosh <marc.deshayes44@gmail.com>
2019-02-18 14:25:21 +0100

    Update Actor.js

---
+++ Actor.js
@@ -0,0 +1,22 @@
+class Rectangle {
+    constructor(name, image) {
+        this._name = name;
+        this._image = image;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get image() {
+        return this._image;
+    }
+
+    set image(value) {
+        this._image = value;
+    }
+}
