ef43ef179d75c51b3ad60b92978ee62646d06779
Minitosh <marc.deshayes44@gmail.com>
2019-02-19 13:47:54 +0100

    Fonctionnel ! :)

---
+++ js/Index.js
@@ -0,0 +1,9 @@
+import Player from "./class/Player.js"
+import Monster from "./class/Monster.js"
+
+let player = new Player("Dylan","20","100","");
+let monster = new Monster("Bolkozor","55","250","");
+
+
+
+monster.fight();
