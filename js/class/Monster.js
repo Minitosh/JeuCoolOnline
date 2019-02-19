9a06f1b244333b9564eeb2b16420aa31211c0eee
Minitosh <marc.deshayes44@gmail.com>
2019-02-19 14:06:34 +0100

    Update Actor.js

--- js/class/Monster.js
+++ js/class/Monster.js
@@ -1,8 +1,8 @@
 import Actor from "./Actor.js"
 
 export default class Monster extends Actor {
-    constructor(name, strength, pv, image) {
-        super(name, strength, pv, image); // appelle le constructeur parent avec le paramètre
+    constructor(name, strength, hp, image) {
+        super(name, strength, hp, image); // appelle le constructeur parent avec le paramètre
         console.log("Monstre crée !");
     }
 
