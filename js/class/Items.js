34b8dac2b4a3da7b4a6c17824108b29c67c711e5
Minitosh <marc.deshayes44@gmail.com>
2019-02-19 14:13:54 +0100

    Update Actor.js

--- js/class/Items.js
+++ js/class/Items.js
@@ -1,5 +1,44 @@
 class Items {
 
+    constructor(name, power, range,  image) {
+        this._name = name;
+        this._strenght = power;
+        this._hp = range;
+        this._image = image;
+        this._power = power;
+        this._range = range;
+    }
 
 
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._name = value;
+    }
+
+    get power() {
+        return this._power;
+    }
+
+    set power(value) {
+        this._power = value;
+    }
+
+    get range() {
+        return this._range;
+    }
+
+    set range(value) {
+        this._range = value;
+    }
+
+    get image() {
+        return this._image;
+    }
+
+    set image(value) {
+        this._image = value;
+    }
 }
