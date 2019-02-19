f9f8769b89f7922a4b299daa3d43983276e89ffb
CortoVilain <corto.vilain@epsi.fr>
2019-02-19 15:03:22 +0100

    ajout fichier html et js

--- js/Index.js
+++ js/Index.js
@@ -1,9 +1,15 @@
 import Player from "./class/Player.js"
 import Monster from "./class/Monster.js"
 
-let player = new Player("Dylan","20","100","");
-let monster = new Monster("Bolkozor","55","250","");
+let player = new Player("Heuss L'enfoiré","20","100","img/a97cb2fdfa5d28d7d4e13f0483218938c9e87338.jfif");
+let monster = new Monster("Nikita","55","250","img/yes.jpg");
 
 
+$(document).ready(function() {
+    $(".namePlayer").text(player.name);
+    $(".nameMonster").text(monster.name);
+    $(".imagePlayer").attr('src', player.image);
+    $(".imageMonster").attr('src', monster.image);
+});
 
 monster.fight();
